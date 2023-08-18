bf6b34b77d84cdae11e8808b03790e99cdac5c52
Jacob Alberty <jacob.alberty@gmail.com>
2023-08-17 21:37:41 -0500

    Add log messages to http startup

--- cmd/gogertd/main.go
+++ cmd/gogertd/main.go
@@ -2,19 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 
 	"github.com/phluxx/gogert/internal/service/config"
 	"github.com/phluxx/gogert/internal/service/v1handler"
 )
 
 func main() {
+	slog.Info("main: building config")
 	// build config and launch handlers
 	cfg, err := config.LoadConfig()
 	if err != nil {
-		panic(err)
+		slog.Error("main: failed to load config", "error", err)
+		os.Exit(1)
 	}
 	h := v1handler.NewHttpHandler(cfg)
 	h.RegisterHandler()
-	http.ListenAndServe(fmt.Sprintf(":%s", cfg.HttpPort), h)
+
+	slog.Info("main: starting http server", "port", cfg.HttpPort)
+	err = http.ListenAndServe(fmt.Sprintf(":%s", cfg.HttpPort), h)
+	if err != nil {
+		slog.Error("main: failed to start http server", "error", err)
+		os.Exit(1)
+	}
 }
